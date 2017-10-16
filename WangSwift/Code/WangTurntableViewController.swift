9ad783521a68934653d3288cf81c2ca02384e560
wang542413041 <542413041@qq.com>
2017-10-16 14:25:13 +0800

    转盘抽奖细节修改

--- WangSwift/Code/WangTurntableViewController.swift
+++ WangSwift/Code/WangTurntableViewController.swift
@@ -25,28 +25,31 @@ class WangTurntableViewController: UIViewController {
         }
         //代码块
         tt.makeStop {
-            let suprise = UIView()
+            
+            let backView = UIView()
+            backView.backgroundColor = UIColor.init(red: 0.9, green: 0.9, blue: 0.9, alpha: 0.6)
+            backView.frame = (UIApplication.shared.keyWindow?.frame)!
+            UIApplication.shared.keyWindow?.addSubview(backView)
+            
+            let suprise = UILabel()
             suprise.frame.size = CGSize(width: 100, height: 100)
-            suprise.center = (UIApplication.shared.keyWindow?.center)!
-            suprise.backgroundColor = .gray
-            UIApplication.shared.keyWindow?.addSubview(suprise)
-            //提示文字
-            let label = UILabel()
-            label.backgroundColor = .white
-            label.textColor = .black
-            label.numberOfLines = 0
-            label.text = "恭喜你，中奖了！"
-            suprise.addSubview(label)
-            label.snp.makeConstraints({ (make) in
-                make.left.right.top.bottom.equalTo(suprise)
+            suprise.center = backView.center
+            suprise.backgroundColor = .white
+            suprise.text = "恭喜你中大奖了！"
+            suprise.textAlignment = .center
+            suprise.textColor = .black
+            suprise.numberOfLines = 0
+            backView.addSubview(suprise)
+            suprise.snp.makeConstraints({ (make) in
+                make.center.equalTo(backView)
+                make.size.equalTo(CGSize(width: 100, height: 100))
             })
             UIView.animate(withDuration: 1.0, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 4, options: UIViewAnimationOptions.curveLinear, animations: {
                 //执行动画
                 suprise.frame.size = CGSize(width: 150, height: 150)
-                suprise.center = (UIApplication.shared.keyWindow?.center)!
             }, completion: { (status) in
                 suprise.frame.size = CGSize(width: 100, height: 100)
-                suprise.removeFromSuperview()
+                backView.removeFromSuperview()
                 if status {
                     print("动画完成")
                 } else {
